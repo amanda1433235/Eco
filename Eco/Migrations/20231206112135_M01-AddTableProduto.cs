5aa964fe1e637d20e8b5cf2b1ca91b565d915522
Amanda <Izadora@DESKTOP-MJSLBVS>
2023-12-06 08:26:29 -0300

    Configuração do banco de dados e criação da tabela Produto

---
+++ Eco/Migrations/20231206112135_M01-AddTableProduto.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Eco.Migrations
+{
+    /// <inheritdoc />
+    public partial class M01AddTableProduto : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Produtos",
+                columns: table => new
+                {
+                    Codigo = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Imagem = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Descricao = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TipoProduto = table.Column<int>(type: "int", nullable: false),
+                    Valor = table.Column<double>(type: "float", nullable: false),
+                    Frete = table.Column<double>(type: "float", nullable: false),
+                    Estoque = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Produtos", x => x.Codigo);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Produtos");
+        }
+    }
+}
---
+++ Eco/Models/Produto.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Eco.Models
+{
+    [Table("Produtos")]
+    public class Produto
+    {
+       
+         [Key]
+            public int Codigo { get; set; }
+
+            [Required(ErrorMessage = "Obrigatório informar o nome do produto!")]
+            public string Nome { get; set; }
+
+            public string Imagem { get; set; }
+
+            [Required(ErrorMessage = "Obrigatório informar a descrição do produto!")]
+            [Display(Name = "Descrição")]
+            public string Descricao { get; set; }
+
+            [Required(ErrorMessage = "Obrigatório informar o tipo de produto!")]
+            [Display(Name = "Tipo")]
+            public TipoProduto TipoProduto { get; set; }
+
+            [Required(ErrorMessage = "Obrigatório informar o valor do produto!")]
+            public double Valor { get; set; }
+
+            [Required(ErrorMessage = "Obrigatório informar o frete do produto!")]
+            public double Frete { get; set; }
+
+            [Required(ErrorMessage = "Obrigatório informar a quantidade no estoque!")]
+            public int Estoque { get; set; }        
+
+    }
+
+    public enum TipoProduto
+    {
+        Vegano = 0,
+        NaoVegano = 1
+    }
+}
